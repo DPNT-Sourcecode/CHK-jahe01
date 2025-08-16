6895cf6972a7796b285efd73448e72466c371311
sbx_user1051 <sbx_user1051@169.254.3.189>
2025-08-16 10:59:41 +0000

    Sat Aug 16 10:59:41 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,17 @@
 from collections import Counter
 from string import ascii_uppercase
 
+UNIT_PRICE = {
+    "A": 50, "B": 30, "C": 20, "D": 15, "E": 40,
+    "F": 10, "G": 20, "H": 10, "I": 35, "J": 60,
+    "K": 70, "L": 90, "M": 15, "N": 40, "O": 10,
+    "P": 50, "Q": 30, "R": 50, "S": 20, "T": 20,
+    "U": 40, "V": 50, "W": 20, "X": 17, "Y": 20,
+    "Z": 21,
+}
+
+
+
 class CheckoutSolution:
 
     def _validate_and_count(self, skus):
@@ -249,5 +260,6 @@ def run_tests_r5():
 # Call it if you want to run immediately
 if __name__ == "__main__":
     run_tests_r5()
+
 
 
